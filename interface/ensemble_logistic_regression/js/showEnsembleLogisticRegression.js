859a356ff30340cba9ee3e46800783ca6e790fd9
Fenwick-Cooper <Fenwick@LittleStick.com>
2025-12-06 19:03:10 +0000

    ELR version 2 with country maps initial commit.

--- interface/ensemble_logistic_regression/js/showEnsembleLogisticRegression.js
+++ interface/ensemble_logistic_regression/js/showEnsembleLogisticRegression.js
@@ -6,16 +6,24 @@ let ELRForecast = new ELRData();		// Create a global ELRData object
 async function ELRSelect() {
 	let region = document.getElementById("regionSelect").value;
 	
-	// Change the sub-region depending upon the region
-	let subRegions;
-	if (region=="Kenya") {
-		subRegions = ["Kiambu","Nairobi","Kajiado-East"];
-	} else if (region=="Ethiopia") {
-		subRegions = ["Konso","Moyale"];
-	} else {
-		console.log("ERROR: Unknown region "+region+" in ELRSelect().");
+	// Each region has a different canvas size
+	let width,height;
+	if (region == "Kenya") {
+		width = 605;
+		height = 770;
+	} else if (region == "Ethiopia") {
+		width = 1130;
+		height = 860;
+	}  else if (region == "Rwanda") {
+		width = 690;
+		height = 596;
 	}
-	updateSubRegionMenu(subRegions);
+	
+	// Set the canvas size
+	document.getElementById("ClimateCanvas").width = width;
+	document.getElementById("ClimateCanvas").height = height;
+	document.getElementById("ELRCanvas").width = width;
+	document.getElementById("ELRCanvas").height = height;
 	
 	// Load the ELR forecast
 	await loadELRForecast();
@@ -38,44 +46,15 @@ async function styleSelect() {
 
 // Change the threshold to plot
 async function thresholdSelect() {
+	
+	// Load the ELR climate for the selected threshold
+	await loadELRClimate();
+	
 	// Draw the ELR forecast
 	await drawPlot();
 }
 
-// Set the sub region menu
-function updateSubRegionMenu(subRegions) {
-	
-	// Select the HTML select menu that we are updating
-	let subRegionSelect = document.getElementById("subRegionSelect");
-	
-	// Record the menu's value before we remove it
-	let subRegion = subRegionSelect.value;
-	
-	// Remove all of the current menu items
-	while (subRegionSelect.hasChildNodes()) {
-		subRegionSelect.removeChild(subRegionSelect.firstChild);
-	}
-	
-	// Add the appropriate menu items
-	let option;
-	for (let i=0; i<subRegions.length;i++) {
-		option = document.createElement("option");
-		option.value = subRegions[i];
-		option.innerHTML = subRegions[i];
-		subRegionSelect.appendChild(option);
-	}
-	
-	// If the original value does not exist.
-	if (!(subRegions.includes(subRegion))) {
-		subRegion = subRegions[subRegions.length-1];	// Pick the final one
-	}
-	
-	// Set the menu to the value it should be
-	subRegionSelect.value = subRegion;
-}
-
 // Set the threshold menu
-// XXX Combine with updateSubRegionMenu into one function
 function updateThresholdMenu(thresholds) {
 	
 	// Select the HTML select menu that we are updating
@@ -171,23 +150,22 @@ async function getPBinEdges() {
 // Loads the currently selected ELR forecast (all lead times and thresholds)
 async function loadELRForecast() {
 	let region = document.getElementById("regionSelect").value;
-	let subRegion = document.getElementById("subRegionSelect").value;
 	let year = document.getElementById("initYearSelect").value;
 	let month = document.getElementById("initMonthSelect").value;
 	let day = document.getElementById("initDaySelect").value;
-	
-	// XXX Quick hack to get the file name
-	let subRegionPath;
-	if ((subRegion=="Konso")||(subRegion=="Kiambu")||(subRegion=="Nairobi")) {
-		subRegionPath = "/county/";
+					
+	let fileName;
+	// XXX Desparate hack
+	if (region == "Rwanda") {
+		fileName = "../ELR_predictions/24h_accumulations/"+region+"/county/GAN_"
+					+year+month.padStart(2,'0')+day.padStart(2,'0')+"_ELR_v1.nc";
 	} else {
-		subRegionPath = "/subcounty/";
+		fileName = "../ELR_predictions/24h_accumulations/"+region+"/subcounty/GAN_"
+					+year+month.padStart(2,'0')+day.padStart(2,'0')+"_ELR_v1.nc";
 	}
-	let fileName = "../ELR_predictions/"+region+subRegionPath+"GAN_"+subRegion+"_"
-					+year+month.padStart(2,'0')+day.padStart(2,'0')+"_logreg.nc";
 	
-	// Load data into the forecastDataObject
-	await ELRForecast.loadELRForecast(fileName);
+	// Load data into the ELRDataObject
+	await ELRForecast.loadELRForecast(fileName,year,month,day);
 	
 	// Update the threshold menu depending upon the available thresholds
 	const thresholds = [];
@@ -195,37 +173,77 @@ async function loadELRForecast() {
 		thresholds[i] = ELRForecast.thresholds[i].toString(10);
 	}
 	updateThresholdMenu(thresholds);
+	
+	// Load the ELR climate for the selected threshold
+	await loadELRClimate();
+}
+
+// Load the ELR climate for the selected threshold
+async function loadELRClimate() {
+	let threshold = document.getElementById("thresholdSelect").value;
+	
+	if (ELRForecast.currentThreshold != threshold) {
+	
+		let month = document.getElementById("initMonthSelect").value;
+		fileName = "../../../ELR/climatological_exceedances/clim_exc_"+threshold+"mmday_"+month+"month.nc";
+		
+		// Load data into the ELRClimate ELRDataObject
+		await ELRForecast.loadELRClimate(fileName, threshold, month);
+	}
 }
 
 async function drawPlot(){
 	
 	// Plot attributes
-	let regionName = document.getElementById("subRegionSelect").value;
+	let regionName = document.getElementById("regionSelect").value;
 	let style = document.getElementById("styleSelect").value;
 	let validTime = document.getElementById("validTimeSelect").value;
 	let threshold = document.getElementById("thresholdSelect").value;
 	
-	// Get the context for plotting
-	const canvas = document.getElementById("myCanvas");
-	const ctx = canvas.getContext("2d");
-	
-	// Erase the canvas
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	
+	// Kenya
 	let x = 2, y=2;			// Location of plot from top left
-	let width = 500;		// Width of plot in pixels
-	let height = 500;		// Height of plot in pixels
+	
+	let width,height;
+	if (regionName == "Kenya") {
+		width = 623; 		// Width of plot in pixels
+		height = 760;		// Height of plot in pixels
+	} else if (regionName == "Ethiopia") {
+		width = 1125; 		// Width of plot in pixels
+		height = 860;		// Height of plot in pixels
+	} else if (regionName == "Rwanda") {
+		width = 683; 		// Width of plot in pixels
+		height = 595;		// Height of plot in pixels
+	}
 	
 	// Must use await unless all of the region shape data is loaded in advance
 	// otherwise the region shape data could be loaded multiple times and corrupted.
 	
 	// Parse the probability bin edges input boxes
 	pBinEdges = await getPBinEdges();
-		
+	
+	// Get the context for plotting
+	let canvas = document.getElementById("ClimateCanvas");
+	let ctx = canvas.getContext("2d");
+	
+	// Erase the canvas
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	
 	// Draw the ELR forecast
-	let plotRect = await ELRForecast.plotExceedenceProbability(ctx, x, y, width, height,
+	let plotRect = await ELRForecast.plotExceedenceClimate(ctx, x, y, width, height,
 														       style, validTime, threshold, 
 														       pBinEdges, regionName);
+	
+	// Get the context for plotting
+	canvas = document.getElementById("ELRCanvas");
+	ctx = canvas.getContext("2d");
+	
+	// Erase the canvas
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	
+	// Draw the ELR forecast
+	plotRect = await ELRForecast.plotExceedenceProbability(ctx, x, y, width, height,
+														   style, validTime, threshold, 
+														   pBinEdges, regionName);
 }
 
 // Function to inform the user what is going on
